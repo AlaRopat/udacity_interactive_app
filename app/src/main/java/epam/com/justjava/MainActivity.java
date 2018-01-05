549e6d4d6393d4d02c6050be66cc8f4261ef9a20
ala_ropat <ala_ropat@epam.com>
2018-01-05 20:57:38 +0300

    add buttons

--- app/src/main/java/epam/com/justjava/MainActivity.java
+++ app/src/main/java/epam/com/justjava/MainActivity.java
@@ -2,12 +2,79 @@ package epam.com.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
+    private int quantity = 0;
+    private int price = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked
+     */
+    public void submitOrder(View view) {
+        display(quantity);
+        int totalPrice = quantity * price;
+        displayMessage("Total: $" + totalPrice + "\n" + "Thank you!");
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method display a coffee price
+     *
+     * @param number common coffee price
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method is called when the plus button is clicked
+     *
+     * @param view
+     */
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+
+    }
+
+    /**
+     * This method is called when the minus button is clicked
+     *
+     * @param view
+     */
+
+    public void decrement(View view) {
+        quantity = (quantity > 0) ? quantity = quantity - 1 : 0;
+
+        display(quantity);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
