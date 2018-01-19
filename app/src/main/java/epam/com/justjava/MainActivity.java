b038984a8e90a0235cd84ef55d8578e354001d49
ala_ropat@epam.com <Life45Dream!>
2018-01-20 00:23:23 +0300

    add editText view

--- app/src/main/java/epam/com/justjava/MainActivity.java
+++ app/src/main/java/epam/com/justjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -13,7 +14,12 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
     private int quantity = 0;
-    private int price = 5;
+    private static int price = 5;
+    private static final int CHOCOLATE_PRICE = 2;
+    private static final int WHIPPED_CREAM_PRICE = 1;
+    private static final int MIN_NUMBER_OF_COFFEE = 0;
+    private static final int MAX_NUMBER_OF_COFFEE = 100;
+    private static final String ORDER_SUMMARY_MESSAGE = "Name: %s\nQuantity %s\nTotal: $ %s\nAdd Whipped cream? %s\nAdd Chocolate? %s\nThank you! ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         display(quantity);
         int totalPrice = calculatePrice();
-        CheckBox checkBox = findViewById(R.id.whipped_cream_checkbox);
-        displayOrderSummary("Quantity " + quantity + "\n" + "Total: $" + totalPrice + "\n" + "Thank you! " + "\n" + "Add Whipped cream? " + checkBox.isChecked());
+        CheckBox whippedCream = findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocolate = findViewById(R.id.chocolate_checkbox);
+        EditText nameInput = findViewById(R.id.name_description_view);
+        displayOrderSummary(String.format(ORDER_SUMMARY_MESSAGE, nameInput.getText(), quantity, totalPrice, whippedCream.isChecked(), chocolate.isChecked()));
     }
 
     /**
@@ -38,7 +46,16 @@ public class MainActivity extends AppCompatActivity {
      */
 
     private int calculatePrice() {
-        return quantity * price;
+        CheckBox whippedCream = findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocolate = findViewById(R.id.chocolate_checkbox);
+        int totalPrice = quantity * price;
+        if (whippedCream.isChecked()) {
+            totalPrice = totalPrice + WHIPPED_CREAM_PRICE;
+        }
+        if (chocolate.isChecked()) {
+            totalPrice = totalPrice + CHOCOLATE_PRICE;
+        }
+        return totalPrice;
     }
 
     /**
@@ -56,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void increment(View view) {
-        quantity = quantity + 1;
+        quantity = (quantity < MAX_NUMBER_OF_COFFEE) ? quantity + 1 : MAX_NUMBER_OF_COFFEE;
         display(quantity);
 
     }
@@ -68,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void decrement(View view) {
-        quantity = (quantity > 0) ? quantity = quantity - 1 : 0;
+        quantity = (quantity > MIN_NUMBER_OF_COFFEE) ? quantity - 1 : MIN_NUMBER_OF_COFFEE;
 
         display(quantity);
     }
